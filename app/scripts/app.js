bf00893d69672b08d870b4c29ad93a30ed1e2d7f
adawsonjc <adawson11@hotmail.co.uk>
2015-07-29 10:54:04 +0100

    Add Sticky. Bootstrap Alignments
    Sticky Added and bootstrap template aligned

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'ui.bootstrap'  
+    'ui.bootstrap',
+    'sticky'
   ])
 /*  .config(function ($routeProvider) {
     $routeProvider
