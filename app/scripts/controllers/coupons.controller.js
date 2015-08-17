80a9a07ebe7f907b5b0660e22981798ea796725c
bridges19 <dbridges01@qub.ac.uk>
2015-08-17 12:01:18 +0100

    Updated message on single coupon
    Added last 4 digits of extracare card number to success message for
    coupon. This card number is hardcoded - need to uncomment the
    location.search() when we can call the service to retrieve the query
    parameter.

--- app/scripts/controllers/coupons.controller.js
+++ app/scripts/controllers/coupons.controller.js
@@ -11,15 +11,20 @@ angular.module('send2CardApp')
     .controller('CouponsController', function ($location, $filter, couponsService, sendToCardFactory, columniseFactory, $scope) {
 
         var coupons = this;
-        var extraCareCardNumber = $location.search().eccardnum;
+        /*
+                var extraCareCardNumber = $location.search().eccardnum;*/
+
+        var extraCareCardNumber = "12345678";
         var couponNumber = $location.search().couponnum;
         coupons.sendCouponOnStartup = false;
         var initialCouponsOnMobileLoad = 1;
         coupons.unSentCouponPath = "images/sendtocard.png";
         coupons.sentCouponPath = "images/sendtocarddone.png";
         coupons.couponPrinted = "images/printed.png";
-    
+        coupons.cardNumber = extraCareCardNumber.substring(extraCareCardNumber.length - 4, extraCareCardNumber.length);
+
         coupons.sendCouponToCard = function () {
+            console.log("EC Card No. = " + coupons.cardNumber);
             return sendToCardFactory.sendCouponToCard(extraCareCardNumber, couponNumber)
                 .then(sendCouponComplete)
                 .catch(sendCouponFailure);
@@ -39,7 +44,7 @@ angular.module('send2CardApp')
             coupons.notYetActionedFilteredMobileColumn = coupons.notYetActionedMobileColumn[0];
             $scope.hideNotYetActionedLoadMore = true;
         }
-        
+
         coupons.readyToUseLoadMoreCoupons = function () {
             coupons.readyToUseFilteredMobileColumn = coupons.readyToUseMobileColumn[0];
             $scope.hideReadyToUseLoadMore = true;
@@ -53,48 +58,48 @@ angular.module('send2CardApp')
         couponsService.getAllCoupons(extraCareCardNumber).then(function (results) {
             coupons.clickedCoupon = $filter('couponFilter')(results.data.CUST_INF_RESP.XTRACARE.CPNS.ROW, couponNumber, false);
             coupons.clickedCoupon.state = 1;
-            var allCoupons = $filter('couponFilter')(results.data.CUST_INF_RESP.XTRACARE.CPNS.ROW,couponNumber, true);
+            var allCoupons = $filter('couponFilter')(results.data.CUST_INF_RESP.XTRACARE.CPNS.ROW, couponNumber, true);
             //allCoupons: List of Redeemable Coupons
             addExpiresSoon(allCoupons);
             //1. Check load_actl_dt and print_actl_dt
             sortByReadyToUse(allCoupons);
             //if they both do not exist, add to NotYetActionedArray
-            
+
             sortCouponsByExpiryDate(coupons.notYetActionedCoupons);
             sortCouponsByExpiryDate(coupons.readyToUseCoupons);
-            
+
             coupons.notYetActionedDesktopColumns = columniseFactory.columnise(coupons.notYetActionedCoupons, 3);
             coupons.notYetActionedTabletColumns = columniseFactory.columnise(coupons.notYetActionedCoupons, 2);
             coupons.notYetActionedMobileColumn = columniseFactory.columnise(coupons.notYetActionedCoupons, 1);
-            coupons.notYetActionedFilteredMobileColumn = coupons.notYetActionedMobileColumn[0].slice(0,initialCouponsOnMobileLoad);
+            coupons.notYetActionedFilteredMobileColumn = coupons.notYetActionedMobileColumn[0].slice(0, initialCouponsOnMobileLoad);
             coupons.notYetActionedLoadMore = coupons.notYetActionedMobileColumn[0].length - initialCouponsOnMobileLoad;
-            if(coupons.notYetActionedLoadMore <= 0){
+            if (coupons.notYetActionedLoadMore <= 0) {
                 $scope.hideNotYetActionedLoadMore = true;
             }
-            
+
             coupons.readyToUseDesktopColumns = columniseFactory.columnise(coupons.readyToUseCoupons, 3);
             coupons.readyToUseTabletColumns = columniseFactory.columnise(coupons.readyToUseCoupons, 2);
             coupons.readyToUseMobileColumn = columniseFactory.columnise(coupons.readyToUseCoupons, 1);
-            coupons.readyToUseFilteredMobileColumn = coupons.readyToUseMobileColumn[0].slice(0,initialCouponsOnMobileLoad);
+            coupons.readyToUseFilteredMobileColumn = coupons.readyToUseMobileColumn[0].slice(0, initialCouponsOnMobileLoad);
             coupons.readyToUseLoadMore = coupons.readyToUseMobileColumn[0].length - initialCouponsOnMobileLoad;
-            if(coupons.readyToUseLoadMore <= 0){
+            if (coupons.readyToUseLoadMore <= 0) {
                 $scope.hideReadyToUseLoadMore = true;
             }
         });
 
-    
-        function sortByReadyToUse(couponList){
+
+        function sortByReadyToUse(couponList) {
             coupons.notYetActionedCoupons = [];
             coupons.readyToUseCoupons = [];
             for (var i = 0; i < couponList.length; i++) {
-               if(couponList[i].load_actl_dt === "" && couponList[i].prnt_actl_dt === ""){
-                   coupons.notYetActionedCoupons.push(couponList[i]);
-               } else {
-                   coupons.readyToUseCoupons.push(couponList[i]);
-               }
+                if (couponList[i].load_actl_dt === "" && couponList[i].prnt_actl_dt === "") {
+                    coupons.notYetActionedCoupons.push(couponList[i]);
+                } else {
+                    coupons.readyToUseCoupons.push(couponList[i]);
+                }
             }
         }
-    
+
         function sortCouponsByExpiryDate(couponList) {
             couponList.sort(function (a, b) {
                 a = new Date(a.expir_dt);
