8f19d5662812e5de56e55411bd564f0afa46c19c
bridges19 <dbridges01@qub.ac.uk>
2015-09-04 15:31:48 +0100

    Committing changes
    Big big changes. Huge commitment.

--- app/scripts/directives/coupon.directive.js
+++ app/scripts/directives/coupon.directive.js
@@ -7,7 +7,6 @@
  * # couponDirective
  */
 angular.module('send2CardApp')
-<<<<<<< HEAD
   .directive('couponDirective', function () {
 
     function link(scope, elem, attrs) {
@@ -49,80 +48,6 @@ angular.module('send2CardApp')
           scope.coupon.cents = scope.coupon.title.substring([3], [scope.coupon.title.length]);
         } else {
           scope.coupon.cents = scope.coupon.title.substring([2], [scope.coupon.title.length]);
-=======
-    .directive('couponDirective', function () {
-
-        function link(scope, elem, attrs) {
-            scope.isHidden = false;
-            scope.isReadyToUse = false;
-
-            scope.collapseSection = function () {
-                var tempIsCollapsed = scope.coupon.isCollapsed;
-                scope.onResetCollapseStateForAll();
-                scope.coupon.isCollapsed = !tempIsCollapsed;
-
-                elem.addClass("expanded-hide-bottom-border");
-                console.log("element class name = " + elem);
-            }
-
-            if (scope.sendCouponOnStartup === 'true') {
-                scope.onSendCouponToCard()
-                    .then(sendCouponComplete)
-                    .catch(sendCouponFailure);
-                scope.sendCouponOnStartup = false;
-            }
-
-            if (scope.coupon.state != 0) {
-                scope.isReadyToUse = true;
-            }
-
-            if (scope.coupon.state == 1) {
-                scope.isHidden = true;
-            }
-
-          if(scope.coupon.amt_type_cd === "D"){
-            scope.coupon.title = scope.coupon.max_redeem_amt;
-            scope.coupon.showDollarSign = true;
-            scope.coupon.showPercentSign = false;
-            scope.coupon.dollar = scope.coupon.title.substring([0],[1]);
-            scope.coupon.cents = scope.coupon.title.substring([2],[scope.coupon.title.length]);
-
-
-
-          } else if (scope.coupon.amt_type_cd==="P"){
-            scope.coupon.showDollarSign = false;
-            scope.coupon.showPercentSign = true;
-            scope.coupon.title=scope.coupon.max_redeem_amt + " off";
-            scope.coupon.dollar = scope.coupon.title.substring([0],[1]);
-
-          }
-
-
-          scope.clickSendCouponToCard = function () {
-                scope.onSendCouponToCard()
-                    .then(sendCouponComplete)
-                    .catch(sendCouponFailure);
-            }
-
-            function sendCouponComplete(newState) {
-                scope.updateState(newState);
-                scope.isHidden = true;
-            }
-
-            scope.printCoupon = function () {
-                window.print();
-            }
-
-            scope.updateState = function (newState) {
-                scope.isReadyToUse = true;
-                scope.coupon.state = newState;
-            }
-
-            function sendCouponFailure(failureState) {
-                scope.coupon.state = failureState;
-            }
-
->>>>>>> ac751c91ed7f6b7f0944dcbc8c60e0d8d60de1ce
         }
         var zeros = '00';
 
