677ea5d8b971fad7a9e8a5e7a4ac24c6290ddec7
pintofguinness <gibberinghand@yahoo.co.uk>
2015-09-11 00:32:47 +0100

    Some more
    hmm not quite right

--- app/scripts/directives/progressbar.directive.js
+++ app/scripts/directives/progressbar.directive.js
@@ -14,47 +14,12 @@ angular.module('send2CardApp')
                     progressBarServiceData: '='
             },
             controller: function ($location, progressBarFactory) {
-         //       var progressBar = this;
-
-                
-           //     var progressBarService = progressBarFactory.progressBarData;
-           //     this.toggleProgressBar = progressBarFactory.display;
-          //      console.log("display: " + progressBarFactory.display);
-        /*        progressBar.showProgressBar = false;*/
-
-                var extraCareCardNumber = $location.search().eccardnum || "12345678";
-                var couponNumber = $location.search().couponnum;
 
                 this.progressBarServiceData = progressBarFactory.getServiceData();
-/*                progressBarFactory.getServiceData().then(function (results) {
-
-                    this.progressBarServiceData = results;
-
-                }).catch(function (error) {
-                              coupons.multiCouponError = error.multiCouponError;
-                });*/
-
-/*                progressBar.hideProgressBarOnCTA = function () {
-                    console.log("show progress bar before setting to true..." + progressBar.showProgressBar);
-                    progressBar.showProgressBar = false;
-                    console.log("show progress bar after setting to true..." + progressBar.showProgressBar);
-                }
-
-                progressBar.incrementProgressBarValue = function () {
-                    var totalLength = progressBar.actionedLength + progressBar.unactionedLength;
-
-                    progressBar.unactionedLength--;
-                    progressBar.actionedLength++;
-
-                    var progressValue = (progressBar.actionedLength / totalLength) * 100;
-                    progressBar.progressBarValue = progressValue;
-                }*/
             },
             controllerAs: 'progressBarController',
             bindToController: true,
             restrict: 'E',
             link: function postLink(scope, element, attrs) {}
         };
-
-
     });
