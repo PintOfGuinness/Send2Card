87c0dfca3ff0acb69e26f62acde30d8ee7637242
adawsonjc <adawson11@hotmail.co.uk>
2015-09-10 09:27:28 +0100

    small

--- test/spec/controllers/coupon.js
+++ test/spec/controllers/coupon.js
@@ -4,12 +4,12 @@ describe('Controller: CouponCtrl', function () {
     
     beforeEach(module('send2CardApp'));
 
-    beforeEach(inject(function ($rootScope, $controller) {
+/*    beforeEach(inject(function ($rootScope, $controller) {
         scope = $rootScope.$new();
         couponsController = $controller('CouponsController', {
             $scope: scope
         });
-    }));
+    }));*/
 
 
     it('should attach a list of awesomeThings to the scope', function () {
