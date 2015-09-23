39453ab2308d9f7e392411c89f78688995d34a62
bridges19 <dbridges01@qub.ac.uk>
2015-09-23 08:17:50 +0100

    adding progress bar savings text factory.
    not complete but taking down the latest code

---
+++ app/scripts/services/progressBarSavingsText.factory.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name send2CardApp.cookieFactory
+ * @description
+ * # cookieFactory
+ * Factory in the send2CardApp.
+ */
+angular.module('send2CardApp')
+    .factory('progressBarSavingsTextFactory', function ($cookies) {
+        // Public API here
+        return {
+            dollarAmountOrBogo: dollarAmountOrBogo,
+
+        };
+
+        function verifyProgressBarText() {
+            // TODO
+            // implement logic from Jen's email to determine
+            // what is shown re: Percent off or Dollar off..
+
+            /*            •	If only dollar amount or BOGO: "That’s $x in savings!”
+                        •	If dollar amount/BOGO plus percent: “That’s over $x in savings!”
+                        •	If only one percent: “That’s x% in savings!”
+                        •	If multiple percents: “That’s over x% in savings!”
+                        o	X = lowest percent*/
+
+
+
+
+        }
+
+
+    });
